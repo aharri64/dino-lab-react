9178c43c213305fcd2f1373b8f72421793cafa2a
aharri64 <haveacompleteday@gmail.com>
2021-02-23 13:17:59 -0700

    set up the dinos acording to lab instructions and used the map function to iterrat through the array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Comment from './Comments'
 
 function App() {
+  const post = {
+    title: "Dinosaurs are awesome",
+    author: "Stealthy Stegosaurus",
+    body: "Check out this body property!"
+  }
+  const comments = [ "First!", "Great post", "Hire this author now!" ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{post.title}</h1> {/* can use variables with curly brackets */}
+      <h3>by {post.author}</h3>
+      <h3>{post.body}</h3>
+      <ul>
+        {comments.map(comment => (
+          <li>{comment}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Comments.js
@@ -0,0 +1,11 @@
+
+function Comment(props) {
+    return(
+        <div>
+            <h2>Comments</h2>
+            <p>{props.comment}</p>
+        </div>
+    )
+}
+
+export default Comment;
